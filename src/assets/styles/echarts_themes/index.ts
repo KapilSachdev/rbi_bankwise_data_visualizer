a0259424094f020ebb81eabba46672089f3cc94a
Kapil Sachdev <kapilsachdev03@gmail.com>
2025-07-13 23:44:51 +0530

    feat: Add new inspired theme for ECharts

--- src/assets/styles/echarts_themes/index.ts
+++ src/assets/styles/echarts_themes/index.ts
@@ -2,6 +2,7 @@ import { chalk } from './chalk';
 import { darker } from './darker';
 import { essos } from './essos'
 import { infographic } from './infographic'
+import { inspired } from './inspired'
 import { purplePassion } from './purple_passion'
 
 export const ECHARTS_THEMES = [
@@ -12,5 +13,5 @@ export const ECHARTS_THEMES = [
   { name: 'default', mode: 'light' },
   { name: 'essos', mode: 'light', themeOptions: essos },
   { name: 'infographic', mode: 'light', themeOptions: infographic },
+  { name: 'inspired', mode: 'light', themeOptions: inspired },
 ];
-
