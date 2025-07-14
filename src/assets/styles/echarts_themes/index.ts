aa3ce8d2d6f703b762d599a00747d3092a1ae14f
Kapil Sachdev <kapilsachdev03@gmail.com>
2025-07-15 01:40:03 +0530

    feat: Add azul theme for ECharts

--- src/assets/styles/echarts_themes/index.ts
+++ src/assets/styles/echarts_themes/index.ts
@@ -6,6 +6,7 @@ import { infographic } from './infographic';
 import { inspired } from './inspired';
 import { purplePassion } from './purple_passion';
 import { v5 } from './v5';
+import { azul } from './azul';
 
 
 export const ECHARTS_THEMES = [
@@ -18,4 +19,5 @@ export const ECHARTS_THEMES = [
   { mode: 'light', name: 'infographic', themeOptions: infographic },
   { mode: 'light', name: 'inspired', themeOptions: inspired },
   { mode: 'light', name: 'v5', themeOptions: v5 },
+  { mode: 'light', name: 'azul', themeOptions: azul },
 ];
