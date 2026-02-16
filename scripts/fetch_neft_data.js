11083e6681b474ed9257214ebbd5fba7b34c13f3
Kapil Sachdev <kapilsachdev03@gmail.com>
2026-02-16 22:43:09 +0530

    fix: Update command-line argument parsing for month

--- scripts/fetch_neft_data.js
+++ scripts/fetch_neft_data.js
@@ -9,8 +9,9 @@ import { getTargetDate } from './utils.js';
  */
 const main = async () => {
   try {
-    // Parse command-line argument: YYYY_MM
-    const { year, month } = getTargetDate(process.argv[2]);
+    // Parse command-line argument: --month=YYYY_MM
+    const monthArg = process.argv.find(arg => arg.startsWith('--month='));
+    const { year, month } = getTargetDate(monthArg);
     // Remove existing file and force re-download
     const force = process.argv.some(arg => ['--force', '-f'].includes(arg));
 
